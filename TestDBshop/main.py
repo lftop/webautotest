914eeacc894ff29ba5970abaedb079d1d8840c0d
lf2020zggboss <59215430+lftop@users.noreply.github.com>
2022-07-04 18:02:25 +0800

    Update main.py

--- TestDBshop/main.py
+++ TestDBshop/main.py
@@ -9,4 +9,5 @@ if __name__=="__main__":
     subprocess.call('allure generate results/report/result/ -o results/report/html --clean', shell=True)
     subprocess.cal1('allure open -h 127.0.0.1 -p  8088 ./results/report/html', shell=True)
 # if __name__=="__main__":
+      print("Helloword")
 #     pytest.main(['-s'])
